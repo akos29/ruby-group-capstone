be1ae79075942839fe82b40209e802ebee0fb88b
jorgegoco <jorgegoco70@gmail.com>
2023-02-21 20:25:44 +0100

    Fix rubocop offenses

--- spec/musicalbum_spec.rb
+++ spec/musicalbum_spec.rb
@@ -7,37 +7,37 @@ describe '#constructor' do
     expect(newalbum).to be_instance_of(MusicAlbum)
   end
 
-describe '#can_be_archived?' do
-  context 'on Spotify and publish date < 10 years' do
-    newalbum = MusicAlbum.new(publish_date: '2-21-2023', on_spotify: true)
-    it 'returns false' do
-      result = newalbum.send(:can_be_archived?)
-      expect(result).to be false
+  describe '#can_be_archived?' do
+    context 'on Spotify and publish date < 10 years' do
+      newalbum = MusicAlbum.new(publish_date: '2-21-2023', on_spotify: true)
+      it 'returns false' do
+        result = newalbum.send(:can_be_archived?)
+        expect(result).to be false
+      end
     end
-  end
 
-  context 'on Spotify and publish date > 10 years' do
-    newalbum = MusicAlbum.new(publish_date: '2-21-2012', on_spotify: true)
-    it 'returns true' do
-      result = newalbum.send(:can_be_archived?)
-      expect(result).to be true
+    context 'on Spotify and publish date > 10 years' do
+      newalbum = MusicAlbum.new(publish_date: '2-21-2012', on_spotify: true)
+      it 'returns true' do
+        result = newalbum.send(:can_be_archived?)
+        expect(result).to be true
+      end
     end
-  end
 
-  context 'not in  Spotify and publish date > 10 years' do
-    newalbum = MusicAlbum.new(publish_date: '2-21-2012', on_spotify: false)
-    it 'returns false' do
-      result = newalbum.send(:can_be_archived?)
-      expect(result).to be false
+    context 'not in  Spotify and publish date > 10 years' do
+      newalbum = MusicAlbum.new(publish_date: '2-21-2012', on_spotify: false)
+      it 'returns false' do
+        result = newalbum.send(:can_be_archived?)
+        expect(result).to be false
+      end
     end
-  end
 
-  context 'not in  Spotify and publish date < 10 years' do
-    newalbum = MusicAlbum.new(publish_date: '2-21-2018', on_spotify: false)
-    it 'returns false' do
-      result = newalbum.send(:can_be_archived?)
-      expect(result).to be false
+    context 'not in  Spotify and publish date < 10 years' do
+      newalbum = MusicAlbum.new(publish_date: '2-21-2018', on_spotify: false)
+      it 'returns false' do
+        result = newalbum.send(:can_be_archived?)
+        expect(result).to be false
+      end
     end
   end
-end 
 end
