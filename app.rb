d4b76c36cd8bd951d1a4c1a453c3f69fb9f54bef
Nabeel Akbar <studiosnabeel@gmail.com>
2023-02-21 14:34:39 +0500

    Add methods to app.rb

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require_relative 'item'
+
 class App 
   def initialize
     @items = []
@@ -6,4 +8,27 @@ class App
     @genres = []
   end
 
+  def list_items
+    @items.each do |item|
+      puts item
+    end  
+  end
+
+  def list_labels
+    @labels.each do |label|
+      puts label
+    end
+  end
+
+  def list_authors
+    @authors.each do |author|
+      puts author
+    end
+  end
+
+  def list_genres
+    @genres.each do |genre|
+      puts genre
+    end
+  end
 end
