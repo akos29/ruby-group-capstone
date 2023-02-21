0bbb57997b0f5746f2025bbe91a00463799e2d8c
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-02-21 16:38:24 +0200

    Add create book method and update main.rb and app.rb

--- main.rb
+++ main.rb
@@ -5,26 +5,49 @@ class Main < App
     puts
     puts 'Please choose a number for
     the following options:'
-    puts '1 - List all items.'
-    puts '2 - List all labels.'
-    puts '3 - List all authors.'
+    puts '1 - List all books.'
+    puts '2 - List all music albums.'
+    puts '3 - List all games.'
     puts '4 - List all genres.'
-    puts '5 - Exit.'
+    puts '5 - List all labels.'
+    puts '6 - List all authors.'
+    puts '7 - Add a book.'
+    puts '8 - Add a music album.'
+    puts '9 - Add a game.'
+    puts '10 - Exit.'
+  end
+
+  def list_methods(choice)
+    case choice
+    when 1
+      list_all_books
+    when 2
+      list_music_albums
+    when 3
+      list_games
+    when 4
+      list_genres
+    when 5
+      list_labels
+    when 6
+      list_authors
+    end
   end
 
   def run
     loop do
       menu
-      case gets.chomp.to_i
-      when 1
-        list_items
-      when 2
-        list_labels
-      when 3
-        list_authors
-      when 4
-        list_genres
-      when 5
+      choice = gets.chomp.to_i
+      case ch
+      when 1..6
+        list_methods(choice)
+      when 7
+        add_book
+      when 8
+        add_music_album
+      when 9
+        add_game
+      when 10
         break
       else
         puts 'Invalid input.'
