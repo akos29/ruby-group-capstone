6139dd732a5ddff754b5c3ed77f152fa19a4a388
jorgegoco <jorgegoco70@gmail.com>
2023-02-21 18:05:18 +0100

    Fix test method add_item

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,5 +1,5 @@
 require './genre'
-require './item'
+require './musicalbum'
 
 describe Genre do
   context 'Testing genre class' do
@@ -9,12 +9,12 @@ describe Genre do
       expect(genre).to have_attributes(name: 'Abc')
       expect(genre).to have_attributes(items: [])
     end
-    # it 'add an item to a genre (add_item method)', positive: true do
-    #   genre = Genre.new name: 'Abc'
-
-    #   genre.add_item item: 'The Artist'
-    #   expect(genre).to have_attributes(items: ['The Artist'])
-    # end
+    it 'add an item to a genre (add_item method)', positive: true do
+      genre = Genre.new name: 'Abc'
+      newalbum = MusicAlbum.new(publish_date: '2-21-2023', on_spotify: true)
+      genre.add_item item: newalbum
+      expect(genre).to have_attributes(items: [newalbum])
+    end
 
     it 'should not create genre without name', negative: true do
       expect { Genre.new }.to raise_error(ArgumentError)
